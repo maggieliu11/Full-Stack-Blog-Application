a151bd2199ecf2dd989c088d37c643ca1e35dfb5
jingkai <liu.jingka@northeastern.edu>
2024-07-30 22:50:49 -0700

    Complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,107 @@
-// src/pages/SignIn.jsx
-import React from 'react';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-const SignIn = () => {
+export default function SignIn() {
+  const [formData, setFormData] = useState({ username: '', email: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim()});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>
-      <h1>Sign In</h1>
-      <p>Please sign in to continue.</p>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 
+            via-purple-500 to-pink-500 rounded-lg text-white'>
+                Maggie's
+              </span>
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>
+              This is a small project of mine. You can sign in with your email and password
+              or with Google.
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='Email'
+                id='email' 
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='*********'
+                id='password' 
+                onChange={handleChange}
+              />
+            </div>
+            <Button 
+              type="submit"
+              className="flex items-center justify-center text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-purple-500 hover:to-indigo-500" disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size='sm'/>
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+              <span>Don't have an account?</span>
+              <Link to='/sign-up' className='text-blue-500'>
+                Sign Up
+              </Link>
+            </div>
+            {errorMessage && (
+             <div className='mt-5 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative' role='alert'>
+              {errorMessage}
+              </div>
+            )}
+        </div>
+      </div>
     </div>
   );
-};
-
-export default SignIn;
+}
