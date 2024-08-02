507ad5239e274c6c3dc0f9af920e747f8b61ba49
jingkai <liu.jingka@northeastern.edu>
2024-08-01 20:19:02 -0700

    complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,26 @@
-//本来是空白文件，下面是我自己加的
-// src/pages/Dashboard.jsx
-import React from 'react';
+import { useEffect, useState} from 'react'
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
-const Dashboard = () => {
+export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>This is the dashboard page.</p>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashProfile />}
     </div>
   );
-};
-
-export default Dashboard;
+}
