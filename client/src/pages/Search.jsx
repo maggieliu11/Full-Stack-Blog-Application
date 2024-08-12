a36c8a322ab3f0e851a5deedf6428e64a9482eb8
jingkai <liu.jingka@northeastern.edu>
2024-08-12 01:20:12 -0700

    Adjusted search function

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -131,9 +131,9 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='Study'>Study</option>
+              <option value='Job'>Job</option>
+              <option value='Life'>Life</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
